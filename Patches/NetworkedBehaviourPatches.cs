36c439ba842226be1238efa6df0a667ed8131d25
Bailey Eaton <xYoshify@gmail.com>
2024-01-14 01:30:12 +1000

    made boombox unregistering more reliable

--- Patches/NetworkedBehaviourPatches.cs
+++ Patches/NetworkedBehaviourPatches.cs
@@ -14,6 +14,20 @@ public class NetworkedBehaviourPatches
     private static void OnDestroyPatch(NetworkBehaviour __instance)
     {
         if (__instance is BoomboxItem boombox)
+        {
+            if (BoomboxManager.IsLookingAtOrHoldingBoombox &&
+                BoomboxManager.LookedAtOrHeldBoombox.NetworkedBoomboxId == __instance.NetworkObjectId)
+            {
+                if (BoomboxManager.LookedAtOrHeldBoombox.IsStreaming)
+                {
+                    BoomboxManager.LookedAtOrHeldBoombox.StopStreamAndPlaybackAndNotify();    
+                }
+                
+                BoomboxManager.OnDroppedBoombox();
+                BoomboxManager.OnLookedAwayFromBoombox();
+            }
+            
             DJNetworkManager.Instance.UnregisterBoomboxServerRpc(boombox.NetworkObjectId);
+        }
     }
 }
