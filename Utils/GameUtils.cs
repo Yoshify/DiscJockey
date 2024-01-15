4a9ddc80cb21fcb8dddc91f6c74964f37b248144
Bailey Eaton <xYoshify@gmail.com>
2024-01-15 17:11:34 +1000

    remove old code from GameUtils.cs

--- Utils/GameUtils.cs
+++ Utils/GameUtils.cs
@@ -2,27 +2,10 @@
 
 public class GameUtils
 {
-    public const string DiscJockeyNameColour = "#1565C0";
-    public const string StandardMessageColour = "#FFFFFF";
-    public const string PlayerNameColour = "#388E3C";
-    private static string DiscJockeyFormattedName => GetColourFormattedText("DiscJockey", DiscJockeyNameColour);
-
     public static string GetPlayerName(ulong playerId)
     {
-        return !StartOfRound.Instance.ClientPlayerList.TryGetValue(playerId, out var playerIndex)
-            ? "UNKNOWN"
-            : StartOfRound.Instance.allPlayerScripts[playerIndex].playerUsername;
-    }
-
-    public static void LogDiscJockeyMessageToLocalChat(string message)
-    {
-        var formattedMessage =
-            $"{DiscJockeyFormattedName}{GetColourFormattedText($"$: {message}", StandardMessageColour)}";
-        HUDManager.Instance.AddChatMessage(formattedMessage);
-    }
-
-    public static string GetColourFormattedText(string text, string hexColour)
-    {
-        return $"<color={hexColour}>{text}</color>";
+        return StartOfRound.Instance.ClientPlayerList.TryGetValue(playerId, out var playerIndex)
+            ? StartOfRound.Instance.allPlayerScripts[playerIndex].playerUsername 
+            : "UNKNOWN";
     }
 }
